ee29b4392206e54b905e56bd98a6f1bd58edaf3d
Philipp Keese <philk.7438@gmail.com>
2020-10-01 16:11:40 +0200

    add substitute stdlib for non-standard functions

---
+++ legacy/sub_stdlib.cpp
@@ -0,0 +1,16 @@
+#include <sub_stdlib.h>
+#include <sstream>
+#include <iomanip>
+
+
+// Non-standard, converts value to a string in str with base
+// Returns beginning of string (str)
+char *itoa(int value, char * str, int base) {
+	// Why write code when you can use std?
+	std::stringstream ss;
+	ss << std::setbase(base);
+	ss << value;
+	const char *cstr = ss.str().c_str();
+	stpcpy(str, cstr);
+	return str;
+}
