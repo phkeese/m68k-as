ecff270cbfe5602f073d06a4a37a5865e4e33ac8
Philipp Keese <philk.7438@gmail.com>
2020-10-01 19:09:44 +0200

    use the objFlag and *use braces*

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 
 int assembleFile(const char fileName[], const char tempName[], const AnsiString workName);
 
+extern bool objFlag;
+
 int main(int argc, char **argv) {
 	std::cout << "Hell awaits in legacy!\n";
 	AnsiString sourcename = argv[1];
 	AnsiString tempname = argv[2];
 	AnsiString workname = argv[3];
 	
+	objFlag = true;
 	std::cout << "Source: " << sourcename << ", Temp: " << tempname << ", Work: " << workname << "\n";
 	assembleFile(sourcename.c_str(), tempname.c_str(), workname);
 }
