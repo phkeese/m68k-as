b1d63bc58065a82728188d8cdbcf9009e44c2370
Philipp Keese <philk.7438@gmail.com>
2020-10-05 16:28:14 +0200

    enable all flags during assembly

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 
 int assembleFile(const char fileName[], const char tempName[], const AnsiString workName);
 
-extern bool objFlag;
-extern bool listFlag;
+extern bool listFlag;	        // True if a listing is desired
+extern bool objFlag;	        // True if an S-Record object code file is desired
+extern bool CEXflag;	        // True is Constants are to be EXpanded
+extern bool BITflag;           // True to assemble bitfield instructions
+extern bool CREflag;           // true adds symbol table to listing
+extern bool MEXflag;           // true expands macro calls in listing
+extern bool SEXflag;           // true expands structured code in listing
+extern bool WARflag;           // true shows Warnings during assembly
 
 int main(int argc, char **argv) {
 	std::cout << "Hell awaits in legacy!\n";
@@ -12,8 +18,16 @@ int main(int argc, char **argv) {
 	AnsiString tempname = argv[2];
 	AnsiString workname = argv[3];
 	
-	objFlag = true;
+
 	listFlag = true;
+	objFlag = true;
+	CEXflag = true;
+	BITflag = true;
+	CREflag = true;
+	MEXflag = true;
+	SEXflag = true;
+	WARflag = true;
+
 	std::cout << "Source: " << sourcename << ", Temp: " << tempname << ", Work: " << workname << "\n";
 	assembleFile(sourcename.c_str(), tempname.c_str(), workname);
 }
